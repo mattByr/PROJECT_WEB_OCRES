7d77bd8618cab0ea7fa4c9b130c5e18df6bc6d60
Inès Belloir <ines.belloir@edu.ece.fr>
2020-11-26 21:29:51 +0100

    flex widget début

--- shorter1/frontend/src/components/dashboard/dashboard.js
+++ shorter1/frontend/src/components/dashboard/dashboard.js
@@ -4,6 +4,8 @@ import { Button } from "react-bootstrap";
 import API from "../../utils/API";
 import WidgetProduct from "../WidgetProduct/addProduct";
 
+import "./dashboard.css";
+
 export class Dashboard extends React.Component {
   disconnect = () => {
     API.logout();
@@ -11,9 +13,22 @@ export class Dashboard extends React.Component {
   };
   render() {
     return (
-      <div className="Dashboard">
+      <div className="row">
         <h1>Dashboard</h1>
-        <WidgetProduct></WidgetProduct>
+        <div className="row_widgets">
+        <WidgetProduct className="row_widget"></WidgetProduct>
+        <WidgetProduct className="row_widget"></WidgetProduct>
+        <WidgetProduct className="row_widget"></WidgetProduct>
+
+        <WidgetProduct className="row_widget"></WidgetProduct>
+        <WidgetProduct className="row_widget"></WidgetProduct>
+        <WidgetProduct className="row_widget"></WidgetProduct>
+
+        <WidgetProduct className="row_widget"></WidgetProduct>
+        <WidgetProduct className="row_widget"></WidgetProduct>
+        <WidgetProduct className="row_widget"></WidgetProduct>
+       
+        </div>
         <Button onClick={this.disconnect} block bsSize="large" type="submit">
           Se déconnecter
         </Button>
