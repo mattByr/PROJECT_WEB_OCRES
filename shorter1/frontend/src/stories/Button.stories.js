eeed85da320cb76fa9127105b45f06de8b0be34c
Matthieu <matthieu.boyer@edu.ece.fr>
2020-11-29 15:23:40 +0100

    arche

--- shorter1/frontend/src/stories/Button.stories.js
+++ shorter1/frontend/src/stories/Button.stories.js
@@ -1,15 +1,9 @@
 import React from 'react';
 
 import { Button } from './Button';
-import WidgetChrono from '../components/WidgetChrono/WidgetChrono';
-import WidgetDistribution from '../components/WidgetDistribution/WidgetDistribution';
-import WidgetLastUsers from '../components/WidgetLastUsers/WidgetLastUsers';
-import WidgetNumberProducts from '../components/WidgetNumberProducts/WidgetNumberProducts';
-import WidgetNumberUsers from '../components/WidgetNumberUsers/WidgetNumberUsers';
-import WidgetSocialMedia from '../components/WidgetSocialMedia/WidgetSocialMedia';
 
 export default {
-  title: 'Example/Widgets',
+  title: 'Example/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -18,11 +12,25 @@ export default {
 
 const Template = (args) => <Button {...args} />;
 
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
 
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
 
-export const widget1 = () =><WidgetChrono />;
-export const widget2 = () =><WidgetDistribution />;
-export const widget3 = () =><WidgetLastUsers />;
-export const widget4 = () =><WidgetNumberProducts />;
-export const widget5 = () =><WidgetNumberUsers />;
-export const widget6 = () =><WidgetSocialMedia />;
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
